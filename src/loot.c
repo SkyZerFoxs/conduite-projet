59756ef0c52bbeac8196800c1fdca093f70b3c77
Raccouard Nathan <s2101613@ic2s122-07.univ-lemans.fr>
2023-04-04 16:37:48 +0200

    Loot terminé

--- src/loot.c
+++ src/loot.c
@@ -10,36 +10,49 @@
 #include <stdlib.h>
 #include <string.h>
 
-objet_t * loot_monstre(liste_t ** liste, int niv_monstre){
-    srand( time( NULL ) );
+int loot_monstre(int niv_monstre){
     int drop = rand() % 3; 
     if(drop==1){
         int item_drop = rand() % 8;
-        if(item_drop=0){
-            return (*liste)->liste[item_drop+niv_monstre];
+        int niv_obj= rand() % 100;
+        if(niv_monstre>=1 && niv_monstre<=10){
+            if(niv_obj>=0 && niv_obj<=4){
+                return item_drop*3+2;
+            }
+            else if(item_drop>=5 && item_drop<=15){
+                return item_drop*3+1;
+            }
+            else{
+                return item_drop*3;
+            }
         }
-        else{
-            return (*liste)->liste[item_drop*3+niv_monstre];
+        else if(niv_monstre>=11 && niv_monstre<=20){
+            if(niv_obj>=0 && niv_obj<=9){
+                return item_drop*3+2;
+            }
+            else if(item_drop>=10 && item_drop<=70){
+                return item_drop*3+1;
+            }
+            else{
+                return item_drop*3;
+            }
         }
-    else{
-        
-    }
+        else if(niv_monstre>=21 && niv_monstre<=25){
+            if(niv_obj>=0 && niv_obj<=49){
+                return item_drop*3+2;
+            }
+            else if(item_drop>=50 && item_drop<=80){
+                return item_drop*3+1;
+            }
+            else{
+                return item_drop*3;
+            }
+        }
     }
 }
 
-objet_t * loot_coffre(liste_t ** liste, int niv_monstre){
-    srand( time( NULL ) );
-    int drop = rand() % 3; 
-    if(drop==1){
-        int item_drop = rand() % 8;
-        if(item_drop=0){
-            return (*liste)->liste[item_drop+niv_monstre];
-        }
-        else{
-            return (*liste)->liste[item_drop*3+niv_monstre];
-        }
-    else{
-        
-    }
-    }
+int loot_coffre(int niv_coffre){
+    int item_drop = rand() % 8;
+    return item_drop*3+niv_coffre;
+
 }
